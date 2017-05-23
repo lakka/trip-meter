41ee8ecdfd8839c51e54210ba9bdb51dc3ff47ed
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2017-05-23 18:34:18 +0300

    Call the api of a nodejs server

--- src/registration-form.jsx
+++ src/registration-form.jsx
@@ -2,17 +2,30 @@ import React from 'baret';
 import Bacon from 'baconjs';
 const emailRegexp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
-const emailBus = new Bacon.Bus()
-const emailDebounced = emailBus.debounce(500)
-const erroneousEmailBus = emailDebounced
+const emailB = new Bacon.Bus()
+const apiResponseB = new Bacon.Bus()
+
+apiResponseB
+  .map(res => res
+    ? <div>Good email!</div>
+    : <div style={{color:'red'}}>Please use the same email account as in your membership application to TRIP!</div>
+  )
+
+const emailDebounced = emailB.debounce(800)
+const erroneousEmailB = emailDebounced
   .filter(email => !email.match(emailRegexp))
-  .map(email => <div style={{color:'red'}}>Erroneus email {email}</div>)
+  .map(email => <div style={{color:'red'}}>Erroneous email</div>)
 
-const goodEmailBus = emailDebounced
+const validatingEmailB = emailDebounced
   .filter(email => email.match(emailRegexp))
-  .map(email => <div>Good email {email}</div>)
+  .filter(e => fetch(`http://localhost:3001/check/${e}`)
+    .then(res => res.json())
+    .then(res => apiResponseB.push(res))
+  )
+  .map(email => <div>Validating email...</div>)
 
-const state = erroneousEmailBus.merge(goodEmailBus)
+
+const state = erroneousEmailB.merge(validatingEmailB).merge(apiResponseB)
 
 const RegistrationForm = () => 
       <div>
@@ -21,7 +34,7 @@ const RegistrationForm = () =>
           <label>
             Email:
             <input type="text"
-              onChange={event => emailBus.push(event.target.value)}
+              onChange={event => emailB.push(event.target.value)}
             />
             <div>{state}</div>
           </label>
