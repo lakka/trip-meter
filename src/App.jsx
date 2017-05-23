6bd8de856222e6e5eda54a98f87d806c860eb5a8
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2017-05-23 18:34:00 +0300

    Rename App.js -> App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'baret';
+import Bacon from 'baconjs';
+import styles from './App.css'
+
+import RegistrationForm from './registration-form'
+
+const App = () =>
+  <RegistrationForm />
+
+
+export default App
