30423bccc67d3db32e477d6f502b3af60b80b153
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2017-05-25 12:23:29 +0300

    Implement a crude session system

--- server/app.js
+++ server/app.js
@@ -5,14 +5,24 @@ const sqlite3 = require('sqlite3').verbose()
 const BPromise = require('bluebird')
 const db = BPromise.promisifyAll(new sqlite3.Database('db.sqlite'))
 const crypto = require('crypto')
-const hash = crypto.createHash('sha256')
 const fs = require('fs')
-const apiToken = fs.readFileSync(__dirname + '/token')
+const path = require('path')
+const apiToken = fs.readFileSync(path.join(__dirname, 'token'))
+const session = BPromise.promisifyAll(require('express-session'))
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'pökkeli'
+}));
+
 app.get('/*',function(req,res,next){
-  res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 
@@ -23,9 +33,16 @@ app.options("/*", function(req, res, next){
   res.send(200);
 });
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
 
+function restrict(req, res, next) {
+  console.log(req.session)
+  if (req.session.email) {
+      next()
+    } else {
+      res.sendStatus(401)
+      res.end()
+    }
+}
 app.get('/check/:email', (req, serverRes) => {
   rp(`https://script.google.com/macros/s/AKfycbyEu4c1yuHpiyo0O_LwQ-8pK6ySTgU7UkA-Wihm_YWhKaPaPtM/exec?type=emailExists&email=${req.params.email}&token=${apiToken}`)
     .then(body => serverRes.send(JSON.parse(body).isRegistered))
@@ -44,6 +61,7 @@ app.get('/exists/:email', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
+  const hash = crypto.createHash('sha256')
   const salt = Math.floor(Math.random()*1000000000)
   hash.update(req.body.pass + salt)
   db.runAsync("INSERT INTO users VALUES ($email, $salt, $hash, 0)", {
@@ -60,13 +78,32 @@ app.post('/register', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-  hash.update(req.body.pass + salt)
-  db.getAsync("SELECT * FROM users WHERE email = ?", req.body.email)
-  .then(row => res.sendStatus(204))
-  .catch(err => {
-    res.sendStatus(500)
-    res.send(err)
+  const hash = crypto.createHash('sha256')
+  if(!req.body.email || !req.body.pass) {
+    res.sendStatus(400)
+    res.end()
+    return
+  }
+  db.get("SELECT * FROM users WHERE email = ?", req.body.email, (err, row) => {
+    if(err) {
+      res.sendStatus(500)
+      res.send(err)
+    } else {
+      if(!row) res.sendStatus(404)
+      else {
+        hash.update(req.body.pass + row.salt)
+        if(hash.digest('hex') === row.password_hash) {
+          req.session.email = req.body.email
+          res.sendStatus(204)
+        } else res.sendStatus(401)
+      }
+    }
+    res.end()
   })
-  .finally(() => res.end())
+})
+
+app.get('/home', restrict, (req, res) => {
+  res.sendStatus(204)
+  res.end()
 })
 app.listen(3001);
