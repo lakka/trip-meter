b06c4b19798ec4b9b19186b261fba082d31c8577
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2017-05-30 23:22:06 +0300

    Add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'baret';
 import Bacon from 'baconjs';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, Redirect, browserHistory } from 'react-router';
 import styles from './App.css'
 
 import RegistrationForm from './registration-form'
 import Home from './home'
+import Login from './login'
 
 const Root = () =>
   <Router history={browserHistory}>
+    <Redirect from='/' to='/home' /> 
     <Route path='/register' component={RegistrationForm} />
     <Route path='/home' component={Home} />
+    <Route path='/login' component={Login} />
   </Router>
 
 
---
+++ src/login.jsx
@@ -0,0 +1,55 @@
+import React from 'baret';
+import Bacon from 'baconjs';
+import { browserHistory } from 'react-router'
+
+const submittedB = new Bacon.Bus()
+const emailB = new Bacon.Bus()
+const passwordB = new Bacon.Bus()
+const errorB = new Bacon.Bus()
+
+const error = Bacon.when(
+  [submittedB, emailB.toProperty(), passwordB.toProperty()],
+  (_, email, pass) => ({email, pass}))
+  .onValue(form =>
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      credentials: 'same-origin',
+      body: JSON.stringify(form)
+    })
+    .then(res => res.ok
+      ? browserHistory.push('/home')
+      : errorB.push(<div>Invalid username or password!</div>)
+    )
+  )
+
+
+const Login = () => 
+  <div>
+    <div>Login:</div>
+    <form onSubmit={e => {
+        e.preventDefault()
+        submittedB.push(e)
+      }}>
+      <label>
+        Email:
+        <input type="text"
+          onChange={e => emailB.push(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input type="password"
+          onChange={e => passwordB.push(e.target.value)}
+        />
+      </label>
+      <div>{errorB}</div>
+      <input type="submit" value="log in" onSubmit={e => submittedB.push(true)} />
+    </form>
+  </div>
+
+export default Login;
+
