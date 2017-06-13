e3aafdb3258db29e9ca8d2bc7f706c96a50ec644
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2017-06-13 13:08:19 +0300

    Add test for submit button

--- test/specs/registration-form.js
+++ test/specs/registration-form.js
@@ -3,16 +3,16 @@ const assert = require('assert');
 describe('Registration page', () => {
   before(() => browser.url('http://localhost:3000/register'))
   it('should have email field', () => {
-    assert.equal(browser.isVisibleWithinViewport('#email'), true)
+    assert.equal(browser.isVisible('#email'), true)
   })
   it('should have password field', () => {
-    assert.equal(browser.isVisibleWithinViewport('#password'), true)
+    assert.equal(browser.isVisible('#password'), true)
   })
   it('should have password again field', () => {
-    assert.equal(browser.isVisibleWithinViewport('#password-again'), true)
+    assert.equal(browser.isVisible('#password-again'), true)
   })
   it('should have disabled submit button', () => {
-    assert.equal(browser.isVisibleWithinViewport('#submit'), true)
+    assert.equal(browser.isVisible('#submit'), true)
     assert.equal(browser.isEnabled('#submit'), false)
   })
   it('should show error on invalid email', () => {
@@ -20,7 +20,7 @@ describe('Registration page', () => {
     const error = $('#email-syntax-error')
     email.setValue('jeesus')
     error.waitForExist(2000)
-    assert.equal(error.isVisibleWithinViewport(), true)
+    assert.equal(error.isVisible(), true)
     email.clearElement()
   })
   it('should show error on existing email', () => {
@@ -28,7 +28,7 @@ describe('Registration page', () => {
     const error = $('#email-exists-error')
     email.setValue('admin@example.com')
     error.waitForExist(2000)
-    assert.equal(error.isVisibleWithinViewport(), true)
+    assert.equal(error.isVisible(), true)
     email.clearElement()
   })
   it('should show error on invalid password', () => {
@@ -36,7 +36,7 @@ describe('Registration page', () => {
     const error = $('#pass-syntax-error')
     pass.setValue('jeesus')
     error.waitForExist(2000)
-    assert.equal(error.isVisibleWithinViewport(), true)
+    assert.equal(error.isVisible(), true)
     pass.clearElement()
   })
   it('should show error when passwords don\'t match', () => {
@@ -46,7 +46,18 @@ describe('Registration page', () => {
     pass.setValue('jeesussimasuu')
     passAgain.setValue('jeesusski')
     error.waitForExist(2000)
-    assert.equal(error.isVisibleWithinViewport(), true)
+    assert.equal(error.isVisible(), true)
     pass.clearElement()
+  })
+  it('should enable submit button on valid input', () => {
+    const email = $('#email')
+    const pass = $('#password')
+    const passAgain = $('#password-again')
+    const submit = $('#submit')
+    email.setValue('lauri.kangassalo@helsinki.fi')
+    pass.setValue('jeesussimasuu')
+    passAgain.setValue('jeesussimasuu')
+    submit.waitForEnabled(5000)
+    assert.equal(submit.isEnabled(), true)
   })
 });
