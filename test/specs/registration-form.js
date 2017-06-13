6985d9adb38c002c134cdb041952957fb26f8f4b
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2017-06-13 12:14:19 +0300

    Use ES6 function definitions

--- test/specs/registration-form.js
+++ test/specs/registration-form.js
@@ -1,21 +1,21 @@
 const assert = require('assert');
 
-describe('Registration page', function() {
+describe('Registration page', () => {
   before(() => browser.url('http://localhost:3000/register'))
-  it('should have email field', function () {
+  it('should have email field', () => {
     assert.equal(browser.isVisibleWithinViewport('#email'), true)
   })
-  it('should have password field', function () {
+  it('should have password field', () => {
     assert.equal(browser.isVisibleWithinViewport('#password'), true)
   })
-  it('should have password again field', function () {
+  it('should have password again field', () => {
     assert.equal(browser.isVisibleWithinViewport('#password-again'), true)
   })
-  it('should have disabled submit button', function () {
+  it('should have disabled submit button', () => {
     assert.equal(browser.isVisibleWithinViewport('#submit'), true)
     assert.equal(browser.isEnabled('#submit'), false)
   })
-  it('should show error on invalid email', function () {
+  it('should show error on invalid email', () => {
     const email = $('#email')
     const error = $('#email-syntax-error')
     email.setValue('jeesus')
@@ -23,7 +23,7 @@ describe('Registration page', function() {
     assert.equal(error.isVisibleWithinViewport(), true)
     email.clearElement()
   })
-  it('should show error on existing email', function () {
+  it('should show error on existing email', () => {
     const email = $('#email')
     const error = $('#email-exists-error')
     email.setValue('admin@example.com')
@@ -31,7 +31,7 @@ describe('Registration page', function() {
     assert.equal(error.isVisibleWithinViewport(), true)
     email.clearElement()
   })
-  it('should show error on invalid password', function () {
+  it('should show error on invalid password', () => {
     const pass = $('#password')
     const error = $('#pass-syntax-error')
     pass.setValue('jeesus')
@@ -39,7 +39,7 @@ describe('Registration page', function() {
     assert.equal(error.isVisibleWithinViewport(), true)
     pass.clearElement()
   })
-  it('should show error when passwords don\'t match', function () {
+  it('should show error when passwords don\'t match', () => {
     const pass = $('#password')
     const passAgain = $('#password-again')
     const error = $('#pass-no-match-error')
